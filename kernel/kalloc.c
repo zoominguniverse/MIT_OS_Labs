cf789124319ff63f7ddd4501ab67568f0c72522b
ZhaoXiaoyu <2821686095@qq.com>
2022-10-18 22:27:16 +0800

    由空闲链表的节点数量和PGSIZE得到空闲内存

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -80,3 +80,19 @@ kalloc(void)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
 }
+int obtain_freememory()
+{
+    struct run * r;
+    int count=0;
+
+    //仿写上面
+    acquire(&kmem.lock);
+    r = kmem.freelist;
+    while(r)
+        kmem.freelist = r->next;
+        count++;
+        r=r->next;
+    release(&kmem.lock);
+    return count*PGSIZE;
+
+}
