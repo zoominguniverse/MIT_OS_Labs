8f06ccf942fa089c749e141b702030919ed57ba3
ZhaoXiaoyu <2821686095@qq.com>
2022-10-18 19:59:49 +0800

    按照打印的格式 打印信息 第一个是syscall 函数编号 第二个是名字 第三个是返回值 由示例中read 1023可以得到

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -142,10 +142,11 @@ syscall(void)
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
     // Use num to lookup the system call function for num, call it,
     // and store its return value in p->trapframe->a0
-    p->trapframe->a0 = syscalls[num]();
+    p->trapframe->a0 = syscalls[num]();//可以作为参考
     int mask=p->trace_mask;
     if(mask>>num&1){
-        printf("Syscall  %s\n",SyscallNames[num-1]);
+        //example 3: syscall read -> 1023
+        printf("%d: syscall %s -> %d",num,SyscallNames[num-1],p->trapframe->a0);
     }
   } else {
     printf("%d %s: unknown sys call %d\n",
