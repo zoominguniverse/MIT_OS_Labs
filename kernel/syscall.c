2f14c3af7b26f115c96b2ccbc9041345ab753746
ZhaoXiaoyu <2821686095@qq.com>
2022-10-18 19:09:38 +0800

    num的取值范围搞混了

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -143,7 +143,7 @@ syscall(void)
     // Use num to lookup the system call function for num, call it,
     // and store its return value in p->trapframe->a0
     p->trapframe->a0 = syscalls[num]();
-    printf("Syscall  %s\n",SyscallNames[num]);
+    printf("Syscall  %s\n",SyscallNames[num-1]);
   } else {
     printf("%d %s: unknown sys call %d\n",
             p->pid, p->name, num);
