b004a8ed9030240a8f9c0061ab63e236bca16b3c
zhaoxiaoyu <iamzhaoxiaoyu@gmail.com>
2022-11-05 19:16:49 +0800

    int 数据类型忘记加上了

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -97,7 +97,7 @@ sys_pgaccess(void)
     //我们可以测试一下 先不对其进行置位 直接写一个值 之后再调用的时候打印一下bitmasks 之后运行一下能否是我们设置的1010
     struct proc *p = myproc();
 
-    res = 0b1010;
+    int res = 0b1010;
 
     if(copyout(p->pagetable, bitmasks, (char *)&res, sizeof(res)) < 0){
       return -1;
