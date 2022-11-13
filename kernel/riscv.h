160909dc52b9e335d7326cb22490b31a95cddca4
zhaoxiaoyu <iamzhaoxiaoyu@gmail.com>
2022-11-13 16:52:58 +0800

    The GCC compiler stores the frame pointer of the currently executing function in the register s0

--- kernel/riscv.h
+++ kernel/riscv.h
@@ -327,6 +327,15 @@ sfence_vma()
   asm volatile("sfence.vma zero, zero");
 }
 
+//The GCC compiler stores the frame pointer of the currently executing function in the register s0
+static inline uint64
+r_fp()
+{
+    uint64 x;
+    asm volatile("mv %0, s0" : "=r" (x) );
+    return x;
+}
+
 typedef uint64 pte_t;
 typedef uint64 *pagetable_t; // 512 PTEs
 
