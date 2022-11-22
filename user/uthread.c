81f045c0d32c9cc0d142ff7327ec3a9bde5bc4a7
zhaoxiaoyu <iamzhaoxiaoyu@gmail.com>
2022-11-22 09:40:35 +0800

    重新定义外部引用 因为switch的是context

--- user/uthread.c
+++ user/uthread.c
@@ -17,7 +17,8 @@ struct thread {
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
-extern void thread_switch(uint64, uint64);
+//extern void thread_switch(uint64, uint64);
+extern void thread_switch(struct *context, struct *context);
               
 void 
 thread_init(void)
