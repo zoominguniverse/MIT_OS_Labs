22e548fcf454b2624215b16bd2be9c4aaaf220b5
zhaoxiaoyu <iamzhaoxiaoyu@gmail.com>
2022-12-20 12:08:13 +0800

    仿照一级间接块的释放 我们采用三层循环释放二级间接块 新增了两个变量用于遍历

--- kernel/fs.c
+++ kernel/fs.c
@@ -460,9 +460,9 @@ bmap(struct inode *ip, uint bn)
 void
 itrunc(struct inode *ip)
 {
-  int i, j;
+  int i, j, k;
   struct buf *bp;
-  uint *a;
+  uint *a,*a2;
 
   for(i = 0; i < NDIRECT; i++){
     if(ip->addrs[i]){
@@ -481,6 +481,28 @@ itrunc(struct inode *ip)
     brelse(bp);
     bfree(ip->dev, ip->addrs[NDIRECT]);
     ip->addrs[NDIRECT] = 0;
+  }
+
+  if(ip->addrs[NDIRECT + 1]) {
+    bp = bread(ip->dev, ip->addrs[NDIRECT + 1]);
+    a = (uint*)bp->data;
+    for(j = 0; j < NINDIRECT; ++j) {
+        if(a[j]) {
+            bp2 = bread(ip->dev, a[j]);
+            a2 = (uint*)bp2->data;
+            for(k = 0; k < NINDIRECT; ++k) {
+                if(a2[k]) {
+                    bfree(ip->dev, a2[k]);
+                }
+            }
+            brelse(bp2);
+            bfree(ip->dev, a[j]);
+            a[j] = 0;
+        }
+    }
+    brelse(bp);
+    bfree(ip->dev, ip->addrs[NDIRECT + 1]);
+    ip->addrs[NDIRECT + 1] = 0;
   }
 
   ip->size = 0;
